850c21e33cb73f4a419a1622793037badf4be407
cjgonzo4 <cjgonzo4@gmail.com>
2015-05-24 15:44:16 -0500

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,18 +1,18 @@
-## Programming Assignment #2 - Carol J. Gonzales
+## Programming Assignment #2 
 ## This first function creates a square matrix which 
 ## returns a list containing a function to do the following
 ## (as in the example to cache mean):
-##      1. Set the Matrix
-##		2. Get the Matrix
-##		3. Set the Inverse
-##		4. Get the Inverse 
-## Returns 1 through 4 functions as a list input to cacheSolve()
+##  1. Set the Matrix
+##  2. Get the Matrix
+##  3. Set the Inverse
+##  4. Get the Inverse 
+##  Returns 1 through 4 functions above as a list to cacheSolve()
 
 makeCacheMatrix <- function(x = matrix()) {
 	inv = NULL
     	set = function(y) {
         x <<- y		## using <<- operator to assign value to an object
-					## different from the current environment
+			## different from the current environment
         inv <<- NULL
         }
         get = function() x
